86b5c4700d8b6ace5b77bc474bcdd24c8b2e9447
zimekk <zimekk@users.noreply.github.com>
2024-04-27 09:34:07 +0000

    deploy: 54eb580b3bada0fb679930c962e4b7442e9368be

---
+++ _next/static/chunks/pages/index-a52b00dd2469fe89.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{9917:(_,n,u)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(8265)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[27,118,215,888,774,179],()=>n(9917)),_N_E=_.O()}]);
