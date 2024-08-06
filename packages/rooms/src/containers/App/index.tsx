37146877f28cbe86e04fe9d395b09f84c8de6a2c
Marcin Makarewicz <marcin@makarewicz.eu>
2024-08-06 23:41:42 +0200

    fix(rooms): room list

--- packages/rooms/src/containers/App/index.tsx
+++ packages/rooms/src/containers/App/index.tsx
@@ -1,29 +1,23 @@
 import { type ChangeEventHandler, useCallback, useMemo, useState } from "react";
 import styles from "./styles.module.scss";
 
-function Table() {
-  const [list] = useState(() => [
-    { name: "Sypialnia 1", size: [4, 3.5] },
-    { name: "Sypialnia 2", size: [4, 3.5] },
-    { name: "Łazienka", size: [3, 2] },
-    { name: "Sypialnia", size: [4, 3.5] },
-    { name: "Łazienka", size: [2.5, 2] },
-    { name: "Garderoba", size: [2.5, 2] },
-    { name: "Pralnia", size: [2.5, 2] },
-    { name: "Schody", size: [2, 2] },
-    { name: "Korytarz", size: [1.5, 4] },
-    { name: "Salon", size: [5, 5] },
-    { name: "Jadalnia", size: [3, 4] },
-    { name: "Kuchnia", size: [3, 4] },
-    { name: "Spiżarnia", size: [2.5, 2] },
-    { name: "Gabinet", size: [4, 3] },
-    { name: "Prysznic", size: [1.5, 2] },
-    { name: "Korytarz", size: [1.5, 4] },
-    { name: "Garderoba", size: [1.5, 2] },
-    { name: "Kotłownia", size: [1.5, 2] },
-    { name: "Wiatrołap", size: [2, 2] },
-    { name: "Garaż", size: [6, 7] },
-  ]);
+interface ItemType {
+  name: string;
+  size: [number, number];
+}
+
+const formatNumber = (size: number) =>
+  new Intl.NumberFormat("pl-PL", {
+    maximumFractionDigits: 2,
+  }).format(size);
+
+const formatArea = (size: [number, number]) =>
+  `${formatNumber(size.reduce((area, size) => area * size, 1))}m2`;
+
+const formatSize = (size: [number, number]) =>
+  `${size.map(formatNumber).join("x")}m`;
+
+function Table({ list }: { list: ItemType[] }) {
   const [selected, setSelected] = useState(() => list.map((_, key) => key));
 
   const filtered = useMemo(() => list, [list]);
@@ -76,8 +70,8 @@ function Table() {
               </label>
             </td>
             <td>{item.name}</td>
-            <td align="right">{`${item.size.join("x")}m`}</td>
-            <td align="right">{`${item.size.reduce((area, size) => area * size, 1)}m2`}</td>
+            <td align="right">{formatSize(item.size)}</td>
+            <td align="right">{formatArea(item.size)}</td>
           </tr>
         ))}
       </tbody>
@@ -111,7 +105,7 @@ function Table() {
             <td></td>
             <td></td>
             <td align="right"></td>
-            <td align="right">{`${item.area}m2`}</td>
+            <td align="right">{formatArea([item.area, 1])}</td>
           </tr>
         ))}
       </tfoot>
@@ -123,7 +117,60 @@ export default function App() {
   return (
     <div>
       <h1>rooms</h1>
-      <Table />
+      <div className={styles.Columns}>
+        <div>
+          <Table
+            list={[
+              { name: "Sypialnia 1", size: [4, 3.5] },
+              { name: "Sypialnia 2", size: [4, 3.5] },
+              { name: "Łazienka", size: [3, 2] },
+              { name: "Sypialnia", size: [4, 3.5] },
+              { name: "Garderoba", size: [2.5, 2] },
+              { name: "Łazienka", size: [2.5, 2] },
+              { name: "Pralnia", size: [2.5, 2] },
+              { name: "Schody", size: [2, 2] },
+              { name: "Korytarz", size: [1.5, 4] },
+              { name: "Salon", size: [5, 5] },
+              { name: "Jadalnia", size: [3, 4] },
+              { name: "Kuchnia", size: [3, 4] },
+              { name: "Spiżarnia", size: [2.5, 2] },
+              { name: "Gabinet", size: [4, 3] },
+              { name: "Prysznic", size: [1.5, 2] },
+              { name: "Korytarz", size: [1.5, 4] },
+              { name: "Garderoba", size: [1.5, 2] },
+              { name: "Kotłownia", size: [1.5, 2] },
+              { name: "Wiatrołap", size: [2, 2] },
+              { name: "Garaż", size: [6, 7] },
+            ]}
+          />
+        </div>
+        <div>
+          <Table
+            list={[
+              { name: "Sypialnia 1", size: [3.25, 2.95] },
+              { name: "", size: [0, 0] },
+              { name: "", size: [0, 0] },
+              { name: "Sypialnia", size: [3.55, 2.95] },
+              { name: "Garderoba", size: [2, 0.6] },
+              { name: "Łazienka", size: [1.85, 2.18] },
+              { name: "Pralnia", size: [1.2, 0.65] },
+              { name: "", size: [0, 0] },
+              { name: "Korytarz", size: [1.2, 4.95] },
+              { name: "Salon", size: [5.25, 5] },
+              { name: "Jadalnia", size: [0, 0] },
+              { name: "Kuchnia", size: [3.1, 2.85 + 0.6] },
+              { name: "", size: [0, 0] },
+              { name: "", size: [0, 0] },
+              { name: "Prysznic", size: [1.95, 1.5] },
+              { name: "Korytarz", size: [2.9, 2] },
+              { name: "Szafa", size: [1.85, 0.65] },
+              { name: "", size: [0, 0] },
+              { name: "", size: [0, 0] },
+              { name: "", size: [0, 0] },
+            ]}
+          />
+        </div>
+      </div>
     </div>
   );
 }
