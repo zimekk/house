0374e9974c8f5074edd6e81a4d26fde57f8b1cc8
Marcin Makarewicz <marcin@makarewicz.eu>
2025-03-29 16:58:34 +0100

    feat(sketch): sketch app

--- packages/sketch/src/index.ts
+++ packages/sketch/src/index.ts
@@ -1,1 +1,7 @@
-export { default } from "./containers/App";
+import dynamic from "next/dynamic";
+import "./index.css";
+
+// https://github.com/mirayatech/NinjaSketch/blob/main/src/types.ts
+const App = dynamic(() => import("./containers/App"), { ssr: false });
+
+export default App;
