464ec1780dd104c5571290321924fe4bf0d88b14
Marcin Makarewicz <marcin@makarewicz.eu>
2025-09-28 14:12:06 +0200

    feat(budget): add items

---
+++ packages/budget/src/data/kitchen.ts
@@ -0,0 +1,44 @@
+export const kitchen = [
+  {
+    category: "Baterie kuchenne",
+    name: "Talis Select M54 PowderSpray Jednouchwytowa bateria kuchenna Semi-Pro, 3jet",
+    url: "https://www.hansgrohe.pl/articledetail-talis-select-m54-powderspray-jednouchwytowa-bateria-kuchenna-semi-pro-3jet-72861000",
+    price: 2223,
+    count: 1,
+  },
+  {
+    category: "Akcesoria kuchenne",
+    name: " A41 Dozownik płynu do naczyń / mydła w płynie",
+    url: "https://www.hansgrohe.pl/articledetail-a41-dozownik-plynu-do-naczyn-mydla-w-plynie-40438000",
+    price: 311,
+    count: 1,
+  },
+  {
+    category: "Zlewozmywaki kuchenne",
+    name: "S71 S712-F450 Zlewozmywak nablatowy 450",
+    url: "https://www.hansgrohe.pl/articledetail-s71-s712-f450-zlewozmywak-nablatowy-450-43305800",
+    price: 3378,
+    count: 1,
+  },
+  {
+    category: "Zlewozmywaki kuchenne",
+    name: "S71 S711-F655 Zlewozmywak nablatowy 180/450",
+    url: "https://www.hansgrohe.pl/articledetail-s71-s711-f655-zlewozmywak-nablatowy-180-450-43309800",
+    price: 5707,
+    count: 0,
+  },
+  {
+    category: "Zlewozmywaki kuchenne",
+    name: "S71 S719-U500 Zlewozmywak podblatowy 500",
+    url: "https://www.hansgrohe.pl/articledetail-s71-s719-u500-zlewozmywak-podblatowy-500-43427800",
+    price: 2622,
+    count: 0,
+  },
+  {
+    category: "Systemy odpływowe",
+    name: "D14-10 Manualny komplet odpływowy z przelewem do pojednczej komory",
+    url: "https://www.hansgrohe.pl/articledetail-d14-10-manualny-komplet-odplywowy-z-przelewem-do-pojednczej-komory-43921800",
+    price: 136,
+    count: 1,
+  },
+];
