73bb806d8361e3aacfd3a4af4939eab9f7d602fe
zimekk <zimekk@gmail.com>
2026-03-29 14:53:41 +0200

    feat(model): add utils

--- packages/model/src/utils/index.ts
+++ packages/model/src/utils/index.ts
@@ -1,10 +1,64 @@
 export type Point = [number, number];
+export type Points = Point[];
+export type Point4 = [Point, Point, Point, Point];
 
-export function shift(a: Point, b: Point): Point {
+const alpha = (35 * Math.PI) / 180;
+
+export const ria = (a: number) => [a, a / Math.tan(alpha), a / Math.sin(alpha)];
+export const rib = (b: number) => [b * Math.tan(alpha), b, b / Math.cos(alpha)];
+export const ric = (c: number) => [c * Math.sin(alpha), c * Math.cos(alpha), c];
+
+export function center(points: Point[]): Point {
+  return points.reduce(
+    (center, [x2, y2]) => {
+      const [x1, y1] = center;
+      return [x1 + x2 / points.length, y1 + y2 / points.length];
+    },
+    [0, 0] as Point,
+  );
+}
+
+export function cross(a: Point, b: Point): Point {
+  const [ax] = a;
+  const [, by] = b;
+
+  return [ax, by];
+}
+
+export function delta(a: Point, b: Point): Point {
   const [ax, ay] = a;
   const [bx, by] = b;
 
-  return [ax + bx, ay + by];
+  return [bx - ax, by - ay];
+}
+
+export function flipy(a: Point): Point {
+  const [ax, ay] = a;
+
+  return [ax, -ay];
+}
+
+export function inner([a, b, c, d]: Point4, m: number): Point4 {
+  const [ax, ay] = a;
+  const [bx, by] = b;
+  const [cx, cy] = c;
+  const [dx, dy] = d;
+
+  return [
+    shift(a, [ax < bx ? m : -m, ay < dy ? m : -m]),
+    shift(b, [bx < ax ? m : -m, by < cy ? m : -m]),
+    shift(c, [cx < dx ? m : -m, cy < by ? m : -m]),
+    shift(d, [dx < cx ? m : -m, dy < ay ? m : -m]),
+  ];
+}
+
+export function length(a: Point, b: Point): number {
+  const [ax, ay] = a;
+  const [bx, by] = b;
+  const adx = Math.abs(ax - bx);
+  const ady = Math.abs(ay - by);
+
+  return Math.max(adx, ady);
 }
 
 export function meter(
@@ -42,26 +96,24 @@ export function middle(a: Point, b: Point): Point {
   return [(ax + bx) / 2, (ay + by) / 2];
 }
 
-export function cross(a: Point, b: Point): Point {
-  const [ax] = a;
-  const [, by] = b;
-
-  return [ax, by];
+export function mirrorh<T extends Points>(points: T, center: Point): T {
+  const [x1, y1] = center;
+  const r = points.map(([x2, y2]) => {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    return [x1 - dx, y2];
+  }) as T;
+  return r;
 }
 
-export function length(a: Point, b: Point): number {
-  const [ax, ay] = a;
-  const [bx, by] = b;
-  const adx = Math.abs(ax - bx);
-  const ady = Math.abs(ay - by);
-
-  return Math.max(adx, ady);
-}
-
-export function flipy(a: Point): Point {
-  const [ax, ay] = a;
-
-  return [ax, -ay];
+export function mirrorv<T extends Points>(points: T, center: Point): T {
+  const [x1, y1] = center;
+  const r = points.map(([x2, y2]) => {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    return [x2, y1 - dy];
+  }) as T;
+  return r;
 }
 
 export function point(a: Point, m: Point = [0, 0]) {
@@ -91,6 +143,13 @@ export function point(a: Point, m: Point = [0, 0]) {
   };
 }
 
-export function rect(a: Point, b: Point): Point[] {
+export function rect(a: Point, b: Point): Point4 {
   return [a, cross(b, a), b, cross(a, b)];
+}
+
+export function shift(a: Point, b: Point): Point {
+  const [ax, ay] = a;
+  const [bx, by] = b;
+
+  return [ax + bx, ay + by];
 }
